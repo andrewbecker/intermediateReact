fd5d493c8f23f62c0b01894fe5b9ee2414d9bfd0
Andrew Becker <andrew.d.becker@lowes.com>
2017-01-31 17:30:13 -0500

    Updated

---
+++ src/components/chart.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import _ from 'lodash';
+
+function average(data) {
+  return _.round(_.sum(data)/data.length);
+}
+
+export default (props) => {
+  const { data, color } = props;
+
+  return (
+    <div>
+      <Sparklines width={180} height={120} data={data}>
+        <SparklinesLine color={color} />
+        <SparklinesReferenceLine type="avg" />
+      </Sparklines>
+      <div>{average(props.data)}</div>
+    </div>
+  );
+}
